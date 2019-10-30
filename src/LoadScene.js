8aa03e435d4d89c9e2441963e1e261c07bd5ec45
Jordan Caumartin <pro.jordanc@gmail.com>
2019-10-30 22:51:13 +0100

    button play functional

--- src/LoadScene.js
+++ src/LoadScene.js
@@ -6,10 +6,20 @@ class LoadScene extends Phaser.Scene{
   }
   preload(){
     this.load.image('background_img', '../Assets/Map/Graveyard/png/BG.png');
-
+    this.load.image('background_img2', '../Assets/Map/Graveyard/png/BG-debut.png');
+    this.load.image('buttonPlay', '../Assets/button/play1.png');
   }
   create(){
-    this.add.image(400, 300, 'background_img').setScale(0.4)
-    this.scene.start(CST.SCENES.MENU, "hello from load scene");
+    this.load.image('background_img', '../Assets/Map/Graveyard/png/BG.png');
+    this.add.image(400, 300, 'background_img').setScale(0.4);
+    this.add.image(400, 300, 'background_img2').setScale(0.8);
+    let playButton = this.add.image(400, 460, 'buttonPlay').setScale(1);
+    // this.sound.play('testmusique',{
+    //   loop : true
+    // })
+
+    playButton.setInteractive();
+
+    playButton.on("pointerdown", ()=> this.scene.start('level_1') && console.log("coucou"))
   }
 }
