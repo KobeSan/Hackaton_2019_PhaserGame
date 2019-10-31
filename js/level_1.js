87795f1c96e5a3f931bdaa1760abf95a77cd1457
Madeline Joubert <madelinejoubert44@gmail.com>
2019-10-31 01:56:22 +0100

    end game if life is equal to zero

--- js/level_1.js
+++ js/level_1.js
@@ -3,11 +3,11 @@ const config = {
   width: window.innerWidth,
   height: window.innerHeight,
   physics: {
-      default: 'arcade',
-      arcade: {
-          gravity: { y: 200 },
-          debug: false
-      }
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 200 },
+      debug: false
+    }
   },
   scene: {
     preload: preload,
@@ -32,55 +32,55 @@ function preload() {
   this.load.image('gainLife', '../Assets/Life/FioleSang.png');
 
   this.load.atlas('vampire', './Assets/Characters/Vampire/vampireWalk.png', './Assets/Characters/Vampire/vampireWalk.json');
-  
+
 
 }
 
 function create() {
-  background = this.add.image(window.innerWidth/2, window.innerHeight/2, 'background').setScrollFactor(0).setDisplaySize(window.innerWidth,window.innerHeight);
+  background = this.add.image(window.innerWidth / 2, window.innerHeight / 2, 'background').setScrollFactor(0).setDisplaySize(window.innerWidth, window.innerHeight);
   player = this.physics.add.sprite(400, 300, 'vampire');
 
   potion = this.physics.add.sprite(500, 435, 'gainLife');
   potion.life = 50
-  player.life = 200 
+  player.life = 50
 
-  var frameNames= this.textures.get('vampire').getFrameNames();
+  var frameNames = this.textures.get('vampire').getFrameNames();
   console.log(frameNames);
-    this.anims.create({
-      key: 'walk',
-      frames: [
-        {
+  this.anims.create({
+    key: 'walk',
+    frames: [
+      {
         key: 'vampire',
         frame: 'walk_000.png'
-        },
-        {
-          key: 'vampire',
-          frame: 'walk_001.png'
-        },
-        {
-          key: 'vampire',
-          frame: 'walk_002.png'
-        },
-        {
-          key: 'vampire',
-          frame: 'walk_003.png'
-        },
-      ],
-      frameRate: 8,
-      repeat: 0
-    });
-  
+      },
+      {
+        key: 'vampire',
+        frame: 'walk_001.png'
+      },
+      {
+        key: 'vampire',
+        frame: 'walk_002.png'
+      },
+      {
+        key: 'vampire',
+        frame: 'walk_003.png'
+      },
+    ],
+    frameRate: 8,
+    repeat: 0
+  });
+
 
   player.setScale(0.4);
   player.setBounce(0.2);
   player.setCollideWorldBounds(true);
   player.body.setGravityY(200);
 
-  map = this.make.tilemap({key: 'map'});
+  map = this.make.tilemap({ key: 'map' });
   tileset = map.addTilesetImage('spritesheet', 'tiles');
   layer = map.createDynamicLayer('top', tileset, 0, 0);
 
-  layer.setCollisionByProperty({collides: true});
+  layer.setCollisionByProperty({ collides: true });
   this.physics.add.collider(player, potion, hitPotion);
   this.physics.add.collider(potion, layer);
   layer.setCollisionByExclusion([-1]);
@@ -101,45 +101,55 @@ function update() {
   if (cursors.left.isDown) {
     player.setVelocityX(-160);
     player.anims.play('walk', true)
-    player.flipX = true 
+    player.flipX = true
   }
   else if (cursors.right.isDown) {
     player.setVelocityX(160);
     player.anims.play('walk', true)
     player.flipX = false
-  }else {
+  } else {
     player.setVelocityX(0);
   };
 
-  if ((cursors.space.isDown || cursors.up.isDown) && player.body.onFloor())
-  {
-      player.body.setVelocityY(-500); // jump up
+  if ((cursors.space.isDown || cursors.up.isDown) && player.body.onFloor()) {
+    player.body.setVelocityY(-500); // jump up
   }
 
-  if (player.life ===  300) {
+  if (player.body.onFloor() < player.setVelocityY(-1)) {
+    this.physics.pause();
+    player.setTint(0xff0000);
+    player.anims.play('walk', false);
+  }
+
+  //***** Gestion affichage vie */
+  if (player.life === 300) {
     this.add.image(1250, 70, 'life').setScrollFactor(0);
     this.add.image(1200, 70, 'life').setScrollFactor(0);
     this.add.image(1150, 70, 'life').setScrollFactor(0);
-  } else if (player.life === 250){
+  } else if (player.life === 250) {
     this.add.image(1250, 70, 'life').setScrollFactor(0);
     this.add.image(1200, 70, 'life').setScrollFactor(0);
     this.add.image(1150, 70, 'middleLife').setScrollFactor(0);
-  }else if (player.life === 200){
+  } else if (player.life === 200) {
     this.add.image(1250, 70, 'life').setScrollFactor(0);
     this.add.image(1200, 70, 'life').setScrollFactor(0);
     this.add.image(1150, 70, 'noLife').setScrollFactor(0);
-  }else if (player.life === 150){
+  } else if (player.life === 150) {
     this.add.image(1250, 70, 'life').setScrollFactor(0);
     this.add.image(1200, 70, 'middleLife').setScrollFactor(0);
     this.add.image(1150, 70, 'noLife').setScrollFactor(0);
-  }else if (player.life === 100){
+  } else if (player.life === 100) {
     this.add.image(1250, 70, 'life').setScrollFactor(0);
     this.add.image(1200, 70, 'noLife').setScrollFactor(0);
     this.add.image(1150, 70, 'noLife').setScrollFactor(0);
-  }else if (player.life === 50){
+  } else if (player.life === 50) {
     this.add.image(1250, 70, 'middleLife').setScrollFactor(0);
     this.add.image(1200, 70, 'noLife').setScrollFactor(0);
     this.add.image(1150, 70, 'noLife').setScrollFactor(0);
+  } else if (player.life === 0) {
+    this.physics.pause();
+    player.setTint(0xff0000);
+    player.anims.play('walk', false);
   }
 
 
@@ -147,9 +157,9 @@ function update() {
   this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
   // make the camera follow the player
   this.cameras.main.startFollow(player);
-  
-  }
-  
-  
+
+}
+
+
 
 
