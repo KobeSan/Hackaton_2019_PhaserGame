7c763af6c4338ab4b9768ed0d2d82c77bfac8467
Martin Klauer <mdklauer@gmail.com>
2019-10-31 13:34:28 +0100

    ajout logo

--- js/TitleScene.js
+++ js/TitleScene.js
@@ -6,19 +6,20 @@ class TitleScene extends Phaser.Scene {
   }
   preload() {
     this.load.image('cimetiere', '../Assets/Map/BG-debut.png');
-    this.load.image('background_img','../Assets/Map/Graveyard/png/BG.png')
-    this.load.image('button', '../Assets/button/play1.png')
-    this.load.audio('music', '../Assets/Music/Musique_fond_video.mp3')
+    this.load.image('background_img','../Assets/Map/Graveyard/png/BG.png');
+    this.load.image('button', '../Assets/button/play1.png');
+    this.load.image('logo', '../Assets/logo.png')
+    this.load.audio('music', '../Assets/Music/Musique_fond_video.mp3');
+
   }
   create() {
-    this.add.image(window.innerWidth/2, window.innerHeight/2, 'background_img').setDisplaySize(window.innerWidth,window.innerHeight);
-    this.add.image(window.innerWidth/2, window.innerHeight/2, 'cimetiere').setDisplaySize(window.innerWidth,window.innerHeight);
-    this.add.text(350, 100, 'Vampire Run');
-    let play = this.add.image(window.innerWidth / 2, 570, 'button').setScale(2);
-    play.setInteractive({ useHandCursor: true });
-    play.on('pointerdown', () => this.scene.switch('level1'));
-
     music = this.sound.add('music');
     music.play();
+    this.add.image(window.innerWidth/2, window.innerHeight/2, 'background_img').setDisplaySize(window.innerWidth,window.innerHeight);
+    this.add.image(window.innerWidth/2, window.innerHeight/2, 'cimetiere').setDisplaySize(window.innerWidth,window.innerHeight);
+    this.add.image(window.innerWidth/2 - 100, window.innerHeight/2 - 200, 'logo').setScale(0.2)
+    let play = this.add.image(window.innerWidth / 2, 620, 'button').setScale(2);
+    play.setInteractive({ useHandCursor: true });
+    play.on('pointerdown', () => this.scene.switch('level1'));
   }
  }
